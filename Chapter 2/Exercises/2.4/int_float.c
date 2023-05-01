45fa185c73444c21c2f7e913afdf8d4299636dde
somerandomguy64 <faizan163123@gmail.com>
2023-05-01 13:53:49 +0100

    2.4 int_float

---
+++ Chapter 2/Exercises/2.4/int_float.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int s, e, v, r, a, l;
+    float i, d, k;
+
+    printf("%d\n%d\n%d\n%d\n%d\n%d\n%f\n%f\n%f\n", s, e, v, r, a, l, i, d, k);
+    return 0;
+}
