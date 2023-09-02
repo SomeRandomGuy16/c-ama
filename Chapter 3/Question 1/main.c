08b456612d45528c0c24927bb41aabfe36cbf1f1
somerandomguy64 <faizan163123@gmail.com>
2023-09-02 18:26:13 +0100

    chapter 3 question 1

---
+++ Chapter 3/Question 1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    int mm, dd, yyyy;
+
+    printf("Enter a date (mm/dd/yyyy):\n");
+    scanf("%d/%d/%d", &mm, &dd, &yyyy);
+    printf("You entered the date: %.4d%.2d%.2d", yyyy, mm, dd);
+}
